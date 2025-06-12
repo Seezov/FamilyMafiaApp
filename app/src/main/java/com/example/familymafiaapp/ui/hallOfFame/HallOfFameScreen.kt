0ee0f8a03b19d3fa3fc85d43b66b59118282c750
Seezov <seezovcenko@gmail.com>
2025-06-12 22:20:09 +0300

    Add icons and new Hall Of Fame Design

--- app/src/main/java/com/example/familymafiaapp/ui/hallOfFame/HallOfFameScreen.kt
+++ app/src/main/java/com/example/familymafiaapp/ui/hallOfFame/HallOfFameScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.LazyColumn
@@ -19,6 +20,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,11 +29,14 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.familymafiaapp.R
 import com.example.familymafiaapp.entities.PlayerPlacements
 import com.example.familymafiaapp.entities.SlotStats
 import com.example.familymafiaapp.entities.Stats
@@ -113,14 +118,7 @@ fun PlayerPlacementsItem(index: Int, placement: PlayerPlacements) {
 
             Spacer(Modifier.height(8.dp))
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text("\uD83E\uDD47: ${placement.firsts}")
-                Text("\uD83E\uDD48: ${placement.seconds}")
-                Text("\uD83E\uDD49: ${placement.thirds}")
-            }
+            PlacementRow(placement)
 
             Spacer(Modifier.height(12.dp))
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
@@ -131,30 +129,115 @@ fun PlayerPlacementsItem(index: Int, placement: PlayerPlacements) {
             }
             Spacer(Modifier.height(8.dp))
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                Text("⭐: ${placement.mvp}")
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_mvp),
+                    contentDescription = "First place",
+                    tint = Color.Unspecified,
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text = placement.mvp.toString(), modifier = Modifier.align(Alignment.CenterVertically))
             }
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text("\uD83D\uDC6E\u200D♂\uFE0F: ${placement.bestSheriff}")
-                Text("\uD83C\uDFA9: ${placement.bestDon}")
-            }
-
-            Spacer(Modifier.height(8.dp))
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text("\uD83D\uDC4D: ${placement.bestCivilian}")
-                Text("\uD83D\uDC4E: ${placement.bestMafia}")
-            }
+            PlacementRoles(placement)
         }
     }
 }
 
+@Composable
+fun PlacementRow(placement: PlayerPlacements) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_first_place),
+                contentDescription = "First place",
+                tint = Color.Unspecified,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = placement.firsts.toString(), modifier = Modifier.align(Alignment.CenterVertically))
+        }
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_second_place),
+                contentDescription = "Second place",
+                tint = Color.Unspecified,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = placement.seconds.toString(), modifier = Modifier.align(Alignment.CenterVertically))
+        }
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_third_place),
+                contentDescription = "Third place",
+                tint = Color.Unspecified,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = placement.thirds.toString(), modifier = Modifier.align(Alignment.CenterVertically))
+        }
+    }
+}
+
+@Composable
+fun PlacementRoles(placement: PlayerPlacements) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_sheriff),
+                contentDescription = "Best Sheriff",
+                modifier = Modifier.size(24.dp),
+                tint = Color.Unspecified  // keep original colors of vector drawable
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = placement.bestSheriff.toString(), modifier = Modifier.align(Alignment.CenterVertically))
+        }
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_don),
+                contentDescription = "Best Don",
+                modifier = Modifier.size(24.dp),
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = placement.bestDon.toString(), modifier = Modifier.align(Alignment.CenterVertically))
+        }
+    }
+
+    Spacer(Modifier.height(8.dp))
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_civilian),
+                contentDescription = "Best Civilian",
+                modifier = Modifier.size(24.dp),
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = placement.bestCivilian.toString(), modifier = Modifier.align(Alignment.CenterVertically))
+        }
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_mafia),
+                contentDescription = "Best Mafia",
+                modifier = Modifier.size(24.dp),
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = placement.bestMafia.toString(), modifier = Modifier.align(Alignment.CenterVertically))
+        }
+    }
+}
 
 @Composable
 fun PlayerWholeStatsScreen(stats: List<Stats>) {
